00bee5a2f3081949004a4a0dc79e62d63ec832a0
kjungyoun <rlawjddbs963@naver.com>
2021-01-18 09:16:33 +0900

    ~2021.01.15 Start Camp

---
+++ homework/src/homework/Variabletest.java
@@ -0,0 +1,12 @@
+package homework;
+
+import java.io.FileInputStream;
+
+public class Variabletest {
+	public static void main(String[] args) {
+		int var1 = 10; // 로컬변수, 초기화를 하지 않고 (연산, 함수인자, 리턴으로) 사용하면 컴파일 에러 발생
+		System.out.println(var1);
+		FileInputStream fis;
+	}
+
+}
