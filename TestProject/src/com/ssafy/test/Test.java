8abca51475dfdbca4072f5702e28b27e36a7fec2
kjungyoun <rlawjddbs963@naver.com>
2021-01-28 23:48:03 +0900

    git test

--- TestProject/src/com/ssafy/test/Test.java
+++ TestProject/src/com/ssafy/test/Test.java
@@ -1,10 +1,14 @@
 package com.ssafy.test;
 
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
-
+			System.out.println("new int String");
+			Scanner sc = new Scanner(System.in);
+			
+		
 	}
 
 }
